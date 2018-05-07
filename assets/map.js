48fa7e239392519730c0ac0abdcdac9c70aaa9ee
Fleck1337 <Fleck2k7@gmail.com>
2018-05-07 11:29:28 +0100

    Create map.js

---
+++ assets/map.js
@@ -0,0 +1,25 @@
+Game.Map = function(tiles) {
+	this._tiles = tiles.length;
+	// Cache the width and height based on length
+	// of the dimensions of the tiles array
+	this._width = tiles.length;
+	this._height = tiles[0].length;
+};
+
+// Standard Getter
+Game.Map.prototype.getWidth = function() {
+	return this._width;
+};
+Game.Map.prototype.getHeight = function() {
+	return this._height;
+};
+
+// gets the tile for a given coordinate set
+Game.Map.prototype.getTile = function(x, y) {
+	// Make sure we are inside the bounds. If not then return null tile.
+	if (x < 0 || x >= this._width || y < 0 || y >= this._height) {
+		return Game.Tile.nullTile;
+	} else {
+		return this._tiles[x][y] || Game.Tile.nullTile;
+	}
+};
