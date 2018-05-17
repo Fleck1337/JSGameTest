d739455933017a3891351e5ca29134fee66519c3
Fleck1337 <Fleck2k7@gmail.com>
2018-05-17 10:41:04 +0100

    Update map.js

--- assets/map.js
+++ assets/map.js
@@ -7,8 +7,10 @@ Game.Map = function(tiles, player) {
 	// Setup the Field of Visions
 	this._fov = [];
 	this.setupFov();
-	// Create a list which will hold the entities
+	// Create a table which will hold the entities
 	this._entities = {};
+	// Create a table which will hold the items
+	this._items = {};
 	// Create the engine and scheduler
 	this._scheduler = new ROT.Scheduler.Simple();
 	this._engine = new ROT.Engine(this._scheduler);
@@ -206,4 +208,36 @@ Game.Map.prototype.updateEntityPosition = function(entity, oldX, oldY, oldZ) {
 	}
 	// Add the entity to the table of entities
 	this._entities[key] = entity;
+};
+
+Game.Map.prototype.getItemsAt = function(x, y, z) {
+	return this._items[x + ',' + y + ',' + z];
+};
+
+Game.Map.prototype.setItemsAt = function(x, y, z, items) {
+	// If our items array is empty, then delete the key from the table
+	var key = x + ',' + y + ',' + z;
+	if (items.length === 0) {
+		if (this._items[key]) {
+			delete this._items[key];
+		}
+	} else {
+		// Simply update the items at that key
+		this._items[key] = items;
+	}
+};
+
+Game.Map.prototype.addItem = function(x, y, z, item) {
+	// If we already have items at that position, simply append the item to the list of items
+	var key = x + ',' + y + ',' + z;
+	if (this._items[key]) {
+		this._items[key].push(item);
+	} else {
+		this._items[key] = [item];
+	}
+};
+
+Game.Map.prototype.addItemAtRandomPosition = function(item, z) {
+	var position = this.getRandomFloorPosition(z);
+	this.addItem(position.x, position.y, position.z, item);
 };
