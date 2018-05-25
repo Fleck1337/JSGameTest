25c3c29276e4426ae279c626551aafe83f8b7e37
Fleck1337 <Fleck2k7@gmail.com>
2018-05-25 14:58:57 +0100

    Update items.js

--- assets/items.js
+++ assets/items.js
@@ -11,7 +11,7 @@ Game.ItemRepository.define('apple', {
 Game.ItemRepository.define('melon', {
 	name: 'melon',
 	character: '%',
-	foreground: 'brightGreen',
+	foreground: 'lightGreen',
 	foodValue: 35,
 	consumptions: 4,
 	mixins: [Game.ItemMixins.Edible]
@@ -31,4 +31,73 @@ Game.ItemRepository.define('rock', {
 	name: 'rock',
 	character: '*',
 	foreground: 'white'
+});
+
+// Weapons
+Game.ItemRepository.define('dagger', {
+	name: 'dagger',
+	character: ')',
+	foreground: 'gray',
+	attackValue: 5,
+	wieldable: true,
+	mixins: [Game.ItemMixins.Equippable]
+}, {
+	disableRandomCreation: true
+});
+
+Game.ItemRepository.define('sword', {
+	name: 'sword',
+	character: ')',
+	foreground: 'white',
+	attackValue: 10,
+	wieldable: true,
+	mixins: [Game.ItemMixins.Equippable]
+}, {
+	disableRandomCreation: true
+});
+
+Game.ItemRepository.define('staff', {
+	name: 'staff',
+	character: ')',
+	foreground: 'yellow',
+	attackValue: 5,
+	defenseValue: 3,
+	wieldable: true,
+	mixins: [Game.ItemMixins.Equippable]
+}, {
+	disableRandomCreation: true
+});
+
+// Wearables
+Game.ItemRepository.define('tunic', {
+	name: 'tunic',
+	character: '[',
+	foreground: 'green',
+	defenseValue: 2,
+	wearable: true,
+	mixins: [Game.ItemMixins.Equippable]
+}, {
+	disableRandomCreation: true
+});
+
+Game.ItemRepository.define('chainmail', {
+	name: 'chainmail',
+	character: '[',
+	foreground: 'white',
+	defenseValue: 4,
+	wearable: true,
+	mixins: [Game.ItemMixins.Equippable]
+}, {
+	disableRandomCreation: true
+});
+
+Game.ItemRepository.define('platemail', {
+	name: 'platemail',
+	character: '[',
+	foreground: 'aliceblue',
+	defenseValue: 6,
+	wearable: true,
+	mixins: [Game.ItemMixins.Equippable]
+}, {
+	disableRandomCreation: true
 });
